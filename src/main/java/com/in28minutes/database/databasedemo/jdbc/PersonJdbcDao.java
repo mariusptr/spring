0a1128e6df07dea59c34bd072356b36f8649c82f
mariusptr <marius.patru@stud.fils.upb.ro>
2020-02-27 22:19:04 +0200

    First commit jdbc

---
+++ src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -0,0 +1,31 @@
+package com.in28minutes.database.databasedemo.jdbc;
+
+import com.in28minutes.database.databasedemo.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> findAll() {
+        return jdbcTemplate.query("SELECT * FROM PERSON", new BeanPropertyRowMapper<Person>(Person.class));
+}
+
+    public Person findByID(int id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM PERSON where id=?", new Object[] {id},
+                new BeanPropertyRowMapper<Person>(Person.class));
+    }
+
+    public int deleteById(int id) {
+        return jdbcTemplate.update("delete FROM PERSON where id=?", new Object[] { id });
+    }
+
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+CREATE TABLE person (
+    id integer not null,
+    name varchar(255),
+    location varchar(255),
+    birth_date timestamp,
+    primary key (id)
+);
+
+INSERT INTO person
+    (id, name, location, birth_date)
+    VALUES (10001, 'Marius', 'Bucharest', sysdate());
+
+    INSERT INTO person
+    (id, name, location, birth_date)
+    VALUES (10002, 'Robert', 'Bucharest', sysdate());
+
+    INSERT INTO person
+    (id, name, location, birth_date)
+    VALUES (10003, 'Gabi', 'Bucharest', sysdate());
